1f3b6531583833ed5cacb4bb345eea8fccaa6805
Federico Canz <canzonierifederico7@gmail.com>
2022-07-11 19:56:09 +0000

    Fixed cicd.yml

--- amk-s3/docker-images/build-s3-opensearch.sh
+++ amk-s3/docker-images/build-s3-opensearch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Check For spark ..."
-if [ "$(ls -A spark/setup/)" ]; then
+if [ "$(ls -A spark-s3-opensearch/setup/)" ]; then
      echo "Spark's found"
 else
     echo "Installing Spark's dependencies "
@@ -11,11 +11,11 @@ else
 fi
 
 echo "Building images docker ..."
-cd elastic
-docker build -t amk-elastic-search .
-cd kibana
-docker build -t amk-kibana .
-cd spark
+# cd elastic
+# docker build -t amk-elastic-search .
+# cd kibana
+# docker build -t amk-kibana .
+cd spark-s3-opensearch
 docker build -t amk-spark-s3-to-s3 .
 cd python
 docker build -t amk-python-s3 .
---
+++ amk-s3/docker-images/build-s3.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo "Check For spark ..."
+if [ "$(ls -A spark-s3/setup/)" ]; then
+     echo "Spark's found"
+else
+    echo "Installing Spark's dependencies "
+    mkdir spark/setup
+    wget https://downloads.apache.org/spark/spark-3.1.2/spark-3.1.2-bin-hadoop3.2.tgz
+    mv spark-3.1.2-bin-hadoop3.2.tgz spark/setup/;
+fi
+
+echo "Building images docker ..."
+cd elastic
+docker build -t amk-elastic-search .
+cd kibana
+docker build -t amk-kibana .
+cd spark-s3
+docker build -t amk-spark-s3 .
+cd python
+docker build -t amk-python-s3 .
+cd grafana
+docker build -t amk-grafana .
+
+# cd logstash
+
+# docker build -t amk-logstash-opensearch .
+echo "Done"
